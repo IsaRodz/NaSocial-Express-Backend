fc6ac4215ed532eef6830fe6cc143f1a35033069
IsaRodz <rodrigueziriarte96@gmail.com>
2020-04-27 18:18:08 -0500

    Version 1.0.1

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,16 +6,14 @@ const {
     getUsers,
     getUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    login,
 } = require("../controllers/users.controllers");
 
-router.route("/")
-    .get(getUsers)
-    .post(createUser)
+router.route("/").get(getUsers).post(createUser);
 
-router.route("/:id")
-    .get(getUser)
-    .put(updateUser)
-    .delete(deleteUser)
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+
+router.route("/login").post(login);
 
 module.exports = router;
